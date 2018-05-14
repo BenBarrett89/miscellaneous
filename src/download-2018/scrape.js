7d2c102fcedca2dc95a9c853ec693b57d27c8e03
Ben Barrett <benjaminbarrett1989@gmail.com>
2018-05-14 08:55:51 +0100

    Do some scraping using request-promise and cheerio

--- src/download-2018/scrape.js
+++ src/download-2018/scrape.js
@@ -1,1 +1,25 @@
-console.log('Scrape the page')
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+
+const getOptions = (
+  parser,
+  uri
+) => ({
+  uri,
+  transform: body => parser.load(body)
+})
+
+const scrapePage = (
+  requestPromise,
+  urlOptions
+) => requestPromise(urlOptions)
+  .then(htmlString => {
+    console.log(htmlString)
+  })
+  .catch(error => {
+    console.log(error)
+})
+
+const options = getOptions(cheerio, `https://downloadfestival.co.uk/artists-a-z/`)
+
+scrapePage(rp, options)
