c7d06112194be48ae3f90be82cf49c922b174701
Ben Barrett <BenBarrett89@users.noreply.github.com>
2019-08-27 11:36:31 +0100

    Download 2018 (#1)
    * Started the download-2018
    
    * Do some scraping using request-promise and cheerio
    
    * Select the artist names from the page suing jQuery selectors
    
    * Do a horrible hack to sort them alphabetically without ‘The’
    
    * Link in the README
    
    * Get the stages and the days (and the times, but these haven’t been released yet)
    
    * Change to a CSV style template string
    
    * Add the CSV and some more README

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Some place to store miscellaneous code
 
 ## Modules
 * [bjorklund](src/bjorklund) - An implementation of the Bjorklund algorithm
+* [download-2018](src/download-2018) - A screenscraper to get the [Download Festival](https://downloadfestival.co.uk/) artists in alphabetical order
 * [multi-bjorklund](src/multi-bjorklund) - Layering multiple Bjorklund rhythms polyrhythmically into 'intensity maps'
 * [music-model](src/music-model) - An attempt at a JavaScript object-oriented model of musical concepts
 
---
+++ src/download-2018/scrape.js
@@ -0,0 +1,56 @@
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+
+const getOptions = (
+  parser,
+  uri
+) => ({
+  uri,
+  transform: body => parser.load(body)
+})
+
+const scrapePage = (
+  requestPromise,
+  urlOptions
+) => {
+  let artistArray = []
+  requestPromise(urlOptions)
+    .then($ => {
+      $('.artist').each((index, element) => {
+        const artist = $(element).find('a').text().trim()
+        const rawDetails = $(element).find('p').text().trim().split(', ')
+        const details = rawDetails
+          .reduce((details, detail) => {
+            const colonIndex = detail.indexOf(`:`)
+            const type = detail.slice(0, colonIndex).toLowerCase()
+            const value = detail.slice(colonIndex + 2, detail.length)
+            return Object.assign(
+              {},
+              details,
+              {[type]: value}
+            )
+          }, {})
+        artistArray.push({
+          name: artist,
+          ...details
+        })
+      })
+    })
+    .then(() => {
+      const reorderedArray = artistArray
+        .map(artist => (
+          artist.name.startsWith('The')
+            ? Object.assign({}, artist, { name: `${artist.name.slice(4, artist.name.length)} (The)`})
+            : artist
+        ))
+        .sort((a, b) => a.name > b.name ? 1 : -1)
+        .forEach(artist => console.log(`"${artist.name}", "${artist.stage}", "${artist.day}"`))
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
+const options = getOptions(cheerio, `https://downloadfestival.co.uk/artists-a-z/`)
+
+scrapePage(rp, options)
