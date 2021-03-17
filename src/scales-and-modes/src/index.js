afc3a616c51fca1947300944b417615f18cf96c4
Ben Barrett <benjamin.barrett@engineering.digital.dwp.gov.uk>
2021-03-17 16:39:20 +0000

    Initial scale and mode data

---
+++ src/scales-and-modes/src/index.js
@@ -0,0 +1,84 @@
+const fs = require('fs')
+const path = require('path')
+
+const data = require('../data/raw.js')
+
+const getInterval = (a, b) => {
+  const aNote = a.index
+  const bNote = b.index < a.index ? b.index + 12 : b.index
+  const distance = bNote - aNote
+  return data.intervallicDistances.find(interval => interval.distance === distance)
+}
+
+const calculate = async ({ data, fs, path }) => {
+  const keys = data.keys.reduce((output, key) => {
+    const notationType = key.pitches.natural || key.pitches.sharps ? 'sharp' : 'flat'
+    const keyNameMajor = `${data.notes[key.notes.major].name[notationType]} Major`
+    const keyNameMinor = `${data.notes[key.notes.minor].name[notationType]} Minor`
+
+    const notes = [{
+      index: key.notes.major,
+      name: data.notes[key.notes.major].name[notationType]
+    }]
+      .concat(data.intervals.absolute.map(interval => {
+        const index = (key.notes.major + interval) % 12
+        const name = data.notes[index].name[notationType]
+        return { index, name }
+      }))
+
+    const modes = data.modes.map((mode, index) => {
+      const modeNotes = notes
+        .concat(notes)
+        .slice(index, index + notes.length)
+        .map((note, noteIndex, array) => Object.assign({}, note, {
+          degree: noteIndex + 1,
+          interval: getInterval(array[0], note)
+        }))
+      const name = `${modeNotes[0].name} ${mode.name}`
+      return {
+        name,
+        type: mode,
+        notes: modeNotes
+      }
+    })
+
+    return output.concat({
+      pitches: key.pitches,
+      major: {
+        name: keyNameMajor,
+        index: key.notes.major
+      },
+      minor: {
+        name: keyNameMinor,
+        index: key.notes.minor
+      },
+      notes,
+      modes
+    })
+  }, [])
+
+  const modes = keys.reduce((output, key, keyIndex) => {
+    const keyModes = key.modes.map((mode, modeIndex) => Object.assign({}, mode, {
+      id: `${keyIndex}${modeIndex}`,
+      mode: modeIndex,
+      key: {
+        major: key.major,
+        minor: key.minor,
+        pitches: key.pitches,
+        notes: key.notes
+      }
+    }))
+    return output.concat(...keyModes)
+  }, [])
+
+  const outputFilePathKeys = path.join(__dirname, '..', 'data', 'keys.json')
+  await fs.writeFileSync(outputFilePathKeys, JSON.stringify(keys, undefined, 2))
+  const outputFilePathModes = path.join(__dirname, '..', 'data', 'modes.json')
+  await fs.writeFileSync(outputFilePathModes, JSON.stringify(modes, undefined, 2))
+}
+
+calculate({
+  data,
+  fs,
+  path
+})
