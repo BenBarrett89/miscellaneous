c58710feb2d0fb28eb0d7b0d573f7e53eb8085dd
BenBarrett89 <benjaminbarrett1989@gmail.com>
2021-08-17 22:07:13 +0100

    Negative harmony 1

---
+++ src/negative-harmony/index.js
@@ -0,0 +1,10 @@
+// get data
+
+// populate select
+
+// select default - handle selection
+
+// handler: handle selection
+// construct circle from selection; duplicate circle array and slice from selection + 12
+// pair negatives as if rotated around circle
+// update view - sharp or flat where required
