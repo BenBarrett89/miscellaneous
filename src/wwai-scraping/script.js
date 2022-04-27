9899d16df639a36310c670731d41f8bf467b71bc
Ben Barrett <benjamin.barrett@engineering.digital.dwp.gov.uk>
2022-04-27 17:18:26 +0100

    Some more WWAI scraping

--- src/wwai-scraping/script.js
+++ src/wwai-scraping/script.js
@@ -1,3 +1,7 @@
+// Change p tags with a links to include class="names"
+// https://weirdwonderfulai.art/resources/disco-diffusion-70-plus-artist-studies/ - artists.json
+// https://weirdwonderfulai.art/resources/disco-diffusion-modifiers - modifiers.json (Howl's Moving Castle didn't work, so did manually)
+// https://weirdwonderfulai.art/resources/anything-punk-modifiers-for-ai-art/ - punks.json
 const anchors = document.querySelectorAll('p.names a')
 const as = []
 anchors.forEach(anchor => as.push({text: anchor.innerHTML, link: anchor.href, hash: anchor.hash}))
